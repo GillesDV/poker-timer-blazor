aafcc12d09c8d217a3d0523fe942611778f655ca
Gilles <Gilles@DESKTOP>
2025-10-09 17:17:13 +0200

    modify blind values, to something more realistic for 100K starting stack

--- PokerTimer/Domain/Constants/BlindValues.cs
+++ PokerTimer/Domain/Constants/BlindValues.cs
@@ -7,14 +7,19 @@
         // Input: how many levels, how many players (+ rebuys), how long should a level / blind last
         // ref: https://www.reddit.com/r/poker/comments/u4pxrx/is_there_a_good_formula_to_calculate_a_tournament
         public static readonly int[,] AllBlinds = {
-                { 100, 200 },
-                { 200, 400 },
-                { 300, 400 },
-                { 400, 800 },
                 { 500, 1_000 },
-                { 600, 1_200 },
-                { 1_000, 1_500 },
                 { 1_000, 2_000 },
+                { 1_500, 3_000 },
+                { 2_000, 4_000 },
+                { 3_000, 6_000 },
+                { 4_000, 8_000 },
+                { 6_000, 12_000 },
+                { 8_000, 16_000 },
+                { 10_000, 20_000 },
+                { 15_000, 30_000 },
+                { 20_000, 40_000 },
+                { 30_000, 60_000 },
+                { 50_000, 100_000 }
             };
     }
 }
